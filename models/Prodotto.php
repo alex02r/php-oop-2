0f377a941dce08101fc769ed7f2755a214bd374b
alex02r <alex.giochi02@gmail.com>
2024-01-31 14:51:05 +0100

    [UPGRADE] Inizializzazione Classe Prodotto

--- models/Prodotto.php
+++ models/Prodotto.php
@@ -1,3 +1,14 @@
 <?php
-    
+    class Prodotto{
+        public $name;
+        public $img;
+        public $price;
+
+        function __construct($_name, $_img, $_price){
+            $this->name = $_name;
+            $this->img = $_img;
+            $this->price = $_price;
+        }
+        
+    }
 ?>
