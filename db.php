c4b7d5d1486ccfa760e24068e05cf3afe70c8dea
alex02r <alex.giochi02@gmail.com>
2024-01-31 15:14:34 +0100

    [UPGRADE] Aggiunti tutti i prodotti nell'array

--- db.php
+++ db.php
@@ -14,5 +14,21 @@
     $products[] = $product_2 = new TypeProduct("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", $img_2, 7.99, "cibo", $animal_1);
 
     $img_3 = "https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg";
-    $products[] = $product_3 = new TypeProduct("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", $img_3, 4.99, "cibo", $animal_2);
+    $products[] = $product_3 = new TypeProduct("Almo Nature Cat Daily Lattina", $img_3, 4.99, "cibo", $animal_2);
+
+    $img_4 = "https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg";
+    $products[] = $product_4 = new TypeProduct("Mangime per Pesci Guppy in Fiocchi", $img_4, 6.99, "cibo", $animal_4);
+
+    $img_5 = "https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg";
+    $products[] = $product_5 = new TypeProduct("Voliera Wilma in Legno", $img_5, 43.99, "accessori", $animal_3);
+
+    $img_6 = "https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg";
+    $products[] = $product_6 = new TypeProduct("Cartucce Filtranti per Filtro EasyCrystal", $img_6, 12.99, "accessori", $animal_4);
+
+    $img_7 = "https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg";
+    $products[] = $product_7 = new TypeProduct("Kong Classic", $img_7, 2.99, "giochi", $animal_1);
+
+    $img_8 = "https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg";
+    $products[] = $product_8 = new TypeProduct("Topini di peluche Trixie", $img_8, 1.99, "giochi", $animal_2);
+
 ?>
