cf7ba6475447a5c6025f9fba2dbb125d04db956b
alex02r <alex.giochi02@gmail.com>
2024-01-31 18:43:56 +0100

    [UPGRADE] Aggiunta tipologia della classe

---
+++ models/Tipologia.php
@@ -0,0 +1,28 @@
+<?php
+    include_once __DIR__."/Prodotto.php";
+
+    class Cibo extends Prodotto{
+        public $title;
+        public $scadenza;
+        function __construct($_name, $_img, $_price, $_title, Categoria $_categoria, $_scadenza){
+            parent::__construct($_name, $_img, $_price, $_categoria );
+            $this->title = $_title;
+            $this->scadenza = $_scadenza;
+        }
+    };
+    class Giochi extends Prodotto{
+        public $title;
+        function __construct($_name, $_img, $_price, Categoria $_categoria, $_title){
+            parent::__construct($_name, $_img, $_price, $_categoria );
+            $this->title = $_title;
+        }
+    };
+    class Accessori extends Prodotto{
+        public $title;
+        function __construct($_name, $_img, $_price, Categoria $_categoria,$_title){
+            parent::__construct($_name, $_img, $_price, $_categoria );
+            $this->title = $_title;
+        }
+    };
+
+?>
