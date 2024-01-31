9fbd349bb6d72399f1bbc03fd65e2228ffedcd19
alex02r <alex.giochi02@gmail.com>
2024-01-31 14:57:21 +0100

    [UPGRADE] Inizializzazione classe TypeProduct

--- models/TypeProduct.php
+++ models/TypeProduct.php
@@ -1,3 +1,13 @@
 <?php
-    
+    include_once __DIR__."/Prodotto.php";
+
+    class TypeProduct extends Prodotto{
+        public $type;
+        public $animal;
+        function __construct($_name, $_img, $_price, $_type, $_animal){
+            parent::__construct($_name, $_img, $_price );
+            $this->type = $_type;
+            $this->animal = $_animal;
+        }
+    }
 ?>
