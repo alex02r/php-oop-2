f894cfe3ce22d08fbdc507c8b897ba4f3b1c5683
alex02r <alex.giochi02@gmail.com>
2024-01-31 15:08:10 +0100

    [FIX] Cambiati valori prodotto 3

--- db.php
+++ db.php
@@ -12,6 +12,7 @@
 
     $img_2 = "https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg";
     $products[] = $product_2 = new TypeProduct("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", $img_2, 7.99, "cibo", $animal_1);
-    
-    
+
+    $img_3 = "https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg";
+    $products[] = $product_3 = new TypeProduct("Almo Nature Holistic Maintenance Medium Large Tonno e Riso", $img_3, 4.99, "cibo", $animal_2);
 ?>
